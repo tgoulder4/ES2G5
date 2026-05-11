0d30567a8246d8a66e173e8de9548ee642142276
Tye <tye.goulder@warwick.ac.uk>
2026-05-11 14:36:20 +0100

    Remove WiFi and server functionality
    Removed WiFi connection and server handling code.

--- P1_Sketch/P1_Sketch.ino
+++ P1_Sketch/P1_Sketch.ino
@@ -2,11 +2,6 @@
 
 #include "Arduino.h"
 #include "DFRobotDFPlayerMini.h"
-#include <WiFi.h>
-
-const char *ssid = "TYE-LAPTOP";
-const char *password = "X3r3+172";
-NetworkServer server(80);
 
 #if (defined(ARDUINO_AVR_UNO) || defined(ESP))   // Using a soft serial port
 #include <SoftwareSerial.h>
@@ -25,34 +20,11 @@ void setup()
   Serial.begin(115200);
 #if (defined ESP32)
   // FPSerial.begin(9600, SERIAL_8N1, /*rx =*/D3, /*tx =*/D2);
-  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/16, /*tx =*/17);
+  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/34, /*tx =*/35);
 #else
   FPSerial.begin(9600);
 #endif
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  server.begin();
-
-  Serial.println();
-  Serial.println(F("DFRobot DFPlayer Mini Demo"));
-  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-  
   if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
@@ -85,7 +57,7 @@ void loop()
     printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
   }
 
-  handleServer();
+  // handleServer();
 }
 
 void printDetail(uint8_t type, int value){
@@ -150,49 +122,3 @@ void printDetail(uint8_t type, int value){
   
 }
 
-void handleServer(){
-    NetworkClient client = server.accept();  // listen for incoming clients
-   if (client) {                     // if you get a client,
-    Serial.println("New Client.");  // print a message out the serial port
-    String currentLine = "";        // make a String to hold incoming data from the client
-    while (client.connected()) {    // loop while the client's connected
-      if (client.available()) {     // if there's bytes to read from the client,
-        char c = client.read();     // read a byte, then
-        Serial.write(c);            // print it out the serial monitor
-        if (c == '\n') {            // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
-
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {  // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /N")) {
-          Serial.println("Received next command.");
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
-  }
-}
